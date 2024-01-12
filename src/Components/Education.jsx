87b0133f357d284628965ed30d9d767f95327481
ksyv <137169322+ksyv@users.noreply.github.com>
2024-01-12 13:43:41 +0000

    add education section

---
+++ src/Components/Education.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Education = () => {
+    return(
+     <section className="light" id="education">
+        <h2>Education</h2>
+    </section>
+  )
+}
+export default Education;
